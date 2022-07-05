c5fd5f436701a9c7188c5f6009da51ea0cb71201
Aleksandr <sashacaha200217@gmail.com>
2022-07-05 15:38:02 +0200

    Update Rectangle.py

--- Rectangle.py
+++ Rectangle.py
@@ -9,12 +9,14 @@ class Rectangle:
 
     def __gt__(self, other):
         return self.square() > other.square()
+
     #
     # def __ge__(self, other):
     #     return self.square() >= other.square()
 
     def __lt__(self, other):
         return self.square() < other.square()
+
     #
     # def __le__(self, other):
     #     return self.square() <= other.square()
@@ -26,18 +28,16 @@ class Rectangle:
     #     return self.square() != other.square()
 
     def __add__(self, other):
-        return self.square() + other.square()
+        return Rectangle(1, self.square() + other.square())
 
     def __mul__(self, other):
-        return self.square() * other
+        return Rectangle(self.width * self.height, other)
 
     def __str__(self):
-        return f'Width = {self.width}\nHeight = {self.height}\nS = {self.square()}\n'
+        return f'S = {self.square()}\n'
 
 
-rect_1 = Rectangle(10, 15)
-rect_2 = Rectangle(5, 10)
+rect_1 = Rectangle(10, 15)  # 150
+rect_2 = Rectangle(5, 10)  # 50
 rect_3 = Rectangle(20, 30)
-print(rect_1)
-print(rect_2 < rect_3)
-
+print(rect_1 * 4)
