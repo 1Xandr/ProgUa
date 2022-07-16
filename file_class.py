73e541f9093ffb396e0eba8f203650650d513e11
Aleksandr <sashacaha200217@gmail.com>
2022-07-16 16:58:45 +0200

    Create file_class.py

---
+++ file_class.py
@@ -0,0 +1,23 @@
+def decorator(func):
+
+    def doc(self):
+        file = open(Product.__name__, 'w')
+        file.write(func)
+        file.close()
+        return file
+    return doc
+
+
+class Product:
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    @decorator
+    def __str__(self):
+        return f'Name = {self.name}\nPrice = {self.price}'
+
+
+apple = Product('apple', 5)
+print(apple)
