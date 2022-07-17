4abb6676dfd9099c488c0a88c2fa8a0a9f6c2b00
Aleksandr <sashacaha200217@gmail.com>
2022-07-17 16:52:13 +0200

    Create decor_logger.py

---
+++ decor_logger.py
@@ -0,0 +1,28 @@
+import time
+
+
+def log(funk):
+
+    def decor(*args):
+
+        def loger(n, file):
+            f = open(file, 'w')
+            start_time = time.time()
+
+            for _ in range(n):
+                funk(*args)
+                f.write(f'\n{time.time() - start_time}')
+
+            return funk(*args)
+
+        return loger
+
+    return decor
+
+
+@log
+def suma(a, b):
+    return a + b
+
+
+print(suma(2, 4)(5, 'log'))
